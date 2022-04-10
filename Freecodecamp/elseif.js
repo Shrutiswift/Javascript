66996c8f6613542f5435a7c36a3b28c79e7810a4
Shruti <shrutiswift@gmail.com>
2022-04-10 23:37:08 +0530

    elseif

---
+++ Freecodecamp/elseif.js
@@ -0,0 +1,12 @@
+function shruti(value) {
+    if (value > 10) {
+        console.log("Greater than 10");
+    }
+
+    else if (value < 5) {
+        console.log("Smaller than 5");
+    } else {
+        console.log("Between 5 and 10");
+    }
+}
+shruti(7);
