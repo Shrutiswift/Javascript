1fd13c4a735bff231028b15fea3856b23a1ab828
Shruti <shrutiswift@gmail.com>
2022-04-10 23:26:58 +0530

    if else

---
+++ Freecodecamp/ifelse.js
@@ -0,0 +1,15 @@
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+
+    if (val > 5) {
+        console.log("Bigger than 5");
+    }
+    else {
+        console.log("5 or Smaller");
+    }
+    // Only change code above this line
+    return result;
+}
+
+testElse(4);
